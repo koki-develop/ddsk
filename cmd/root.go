3e8a82ebfe13583bd39ab0a5bc67419ecef3ee0e
koki-develop <kou.pg.0131@gmail.com>
2023-08-21 10:28:27 +0900

    Implement ddsk cli

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,22 @@ package cmd
 import (
 	"os"
 
+	"github.com/koki-develop/ddsk/internal/ddsk"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use: "CLI_NAME", // TODO
+	Use: "ddsk",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		w := cmd.OutOrStdout()
+		ddsk := ddsk.New()
+
+		if err := ddsk.Run(w); err != nil {
+			return err
+		}
+
+		return nil
+	},
 }
 
 func Execute() {
