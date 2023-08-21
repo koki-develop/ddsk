edc11a9ad463303b711a948dfe509956a9e36441
koki-develop <kou.pg.0131@gmail.com>
2023-08-21 11:14:44 +0900

    Implement colorize output

--- internal/ddsk/ddsk.go
+++ internal/ddsk/ddsk.go
@@ -1,6 +1,7 @@
 package ddsk
 
 import (
+	"bytes"
 	"io"
 	"math/rand"
 	"strings"
@@ -29,31 +30,65 @@ func New(cfg *Config) *DDSK {
 }
 
 func (d *DDSK) Run(w io.Writer) error {
-	cur := new(strings.Builder)
+	cur := new(bytes.Buffer)
 
 	for {
-		var next string
-		if rand.Intn(2) == 0 {
-			next = dd
-		} else {
-			next = sk
-		}
-		if _, err := w.Write([]byte(next)); err != nil {
-			return err
+		next := d.choose()
+		if !d.config.Color {
+			if _, err := w.Write([]byte(next)); err != nil {
+				return err
+			}
 		}
 
 		cur.WriteString(next)
 		if cur.Len() > targetLen {
+			if d.config.Color {
+				if _, err := io.Copy(w, cur); err != nil {
+					return err
+				}
+			}
 			cur.Reset()
+			continue
 		}
 
 		if cur.String() == strings.Repeat(target, targetRepeat) {
-			if _, err := w.Write([]byte(love)); err != nil {
-				return err
+			if d.config.Color {
+				rainbowColors := []string{"\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m"}
+
+				for i, c := range []rune(cur.String()) {
+					if _, err := w.Write([]byte(rainbowColors[i%len(rainbowColors)])); err != nil {
+						return err
+					}
+					if _, err := w.Write([]byte(string(c))); err != nil {
+						return err
+					}
+				}
+
+				if _, err := w.Write([]byte("\x1b[1m\x1b[91m")); err != nil {
+					return err
+				}
+				if _, err := w.Write([]byte(love)); err != nil {
+					return err
+				}
+
+				if _, err := w.Write([]byte("\x1b[0m")); err != nil {
+					return err
+				}
+			} else {
+				if _, err := w.Write([]byte(love)); err != nil {
+					return err
+				}
 			}
 			break
 		}
 	}
 
 	return nil
+}
+
+func (*DDSK) choose() string {
+	if rand.Intn(2) == 0 {
+		return dd
+	}
+	return sk
 }
