e9bfed9a3e041a2ef2d75d49026bb18669cef569
koki-develop <kou.pg.0131@gmail.com>
2023-08-21 10:28:19 +0900

    Implement DDSK.Run()

---
+++ internal/ddsk/ddsk.go
@@ -0,0 +1,53 @@
+package ddsk
+
+import (
+	"io"
+	"math/rand"
+	"strings"
+)
+
+const (
+	dd   = "ドド"
+	sk   = "スコ"
+	love = "ラブ注入♡\n"
+
+	target       = dd + sk + sk + sk
+	targetRepeat = 3
+	targetLen    = len(target) * targetRepeat
+)
+
+type DDSK struct{}
+
+func New() *DDSK {
+	return &DDSK{}
+}
+
+func (*DDSK) Run(w io.Writer) error {
+	cur := new(strings.Builder)
+
+	for {
+		var next string
+		if rand.Intn(2) == 0 {
+			next = dd
+		} else {
+			next = sk
+		}
+		if _, err := w.Write([]byte(next)); err != nil {
+			return err
+		}
+
+		cur.WriteString(next)
+		if cur.Len() > targetLen {
+			cur.Reset()
+		}
+
+		if cur.String() == strings.Repeat(target, targetRepeat) {
+			if _, err := w.Write([]byte(love)); err != nil {
+				return err
+			}
+			break
+		}
+	}
+
+	return nil
+}
